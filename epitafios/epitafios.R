b06aaee196a8bc3ac085c936a55119b0e3020a0c
fede2001 <fede2001@hotmail.com>
2015-11-07 17:13:20 -0500

    agrego el paquete tm y funciona el filtro de terminos frecuentes

--- epitafios/epitafios.R
+++ epitafios/epitafios.R
@@ -1,8 +1,22 @@
+install.packages('tm')
+require('tm')
+
+# usando el paquete tm
 epitafios <- read.csv2("data_epitafios.csv")
+corpus <- VCorpus(VectorSource((epitafios)))
+dtm <- DocumentTermMatrix(corpus)
+findFreqTerms(dtm,5)
+
+#------- experimentos ----------
+epitafios <- read.csv2("data_epitafios.csv")
+#epitafios <- c("mi gran amigo", "mi hermano querido", "mi mi mi")
 x <- (epitafios[1])
 x
+
+epitafios
+
 # no logro que separe las palabras
-palabras <- strsplit(x, " ")
+palabras <- strsplit(as.character(x), " ")
 
 palabras
 attributes(palabras)
